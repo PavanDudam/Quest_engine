0ce292ce5abc81c680ef2b580a0dad442ca8a26f
PavanDudam <pavandudam031@gmail.com>
2024-11-07 11:40:34 +0530

    Comments removed

--- quesytEngine_db.py
+++ quesytEngine_db.py
@@ -2,10 +2,8 @@ from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
 import os
 
-# Load environment variables
 load_dotenv(find_dotenv())
 
-# Initialize MongoDB client
 mongo_username = os.getenv("MONGO_USERNAME")
 mongo_password = os.getenv("MONGO_PASSWORD")
 mongo_cluster = os.getenv("MONGO_CLUSTER")
@@ -16,24 +14,13 @@ db = client["quest_Engine"]
 collection = db["collection_one"]
 
 def save_query_result(query, result, data_preview):
-    """
-    Saves a query result to MongoDB with all keys converted to strings.
-    """
-    # Convert data_preview keys to strings
     data_preview_str_keys = {str(k): v for k, v in data_preview.items()}
-
-    # Document to be inserted
     doc = {
         "query": query,
         "result": result,
         "data_preview": data_preview_str_keys
     }
-
     collection.insert_one(doc)
 
-
 def get_recent_results(limit=5):
-    """
-    Retrieves the most recent query results from MongoDB.
-    """
     return list(collection.find().limit(limit))
